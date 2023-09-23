f9ca01539143e0179c42a9832313c027997be606
shlCui <cui_sl@qq.com>
2023-09-23 21:30:11 +0800

    使用 g2o solver 进行原生位姿图优化 和 李代数上的位姿图优化。

---
+++ ch10/pose_graph_g2o_SE3.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <g2o/types/slam3d/types_slam3d.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/solvers/eigen/linear_solver_eigen.h>
+
+using namespace std;
+
+/************************************************
+ * 本程序演示如何用g2o solver进行位姿图优化
+ * sphere.g2o是人工生成的一个Pose graph，我们来优化它。
+ * 尽管可以直接通过load函数读取整个图，但我们还是自己来实现读取代码，以期获得更深刻的理解
+ * 这里使用g2o/types/slam3d/中的SE3表示位姿，它实质上是四元数而非李代数.
+ * **********************************************/
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        cout << "Usage: pose_graph_g2o_SE3 sphere.g2o" << endl;
+        return 1;
+    }
+    ifstream fin(argv[1]);
+    if (!fin) {
+        cout << "file " << argv[1] << " does not exist." << endl;
+        return 1;
+    }
+
+    // 设定g2o    
+    typedef g2o::BlockSolver<g2o::BlockSolverTraits<6, 6>> BlockSolverType; // SE3 对应的李代数是 6 维向量
+    typedef g2o::LinearSolverEigen<BlockSolverType::PoseMatrixType> LinearSolverType;
+    auto solver = new g2o::OptimizationAlgorithmLevenberg(
+        g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
+    g2o::SparseOptimizer optimizer;     // 图模型
+    optimizer.setAlgorithm(solver);   // 设置求解器
+    optimizer.setVerbose(true);       // 打开调试输出
+
+    int vertexCnt = 0, edgeCnt = 0; // 顶点和边的数量
+    while (!fin.eof()) {
+        string name;
+        fin >> name;
+        if (name == "VERTEX_SE3:QUAT") {
+            // SE3 顶点 位姿：平移向量 + 四元数
+            g2o::VertexSE3 *v = new g2o::VertexSE3();
+            int index = 0;
+            fin >> index;
+            v->setId(index);
+            v->read(fin); // 从 fin 流中读取 7 个数（平移向量 + 四元数）https://docs.ros.org/en/fuerte/api/re_vision/html/vertex__se3__quat_8cpp_source.html
+            optimizer.addVertex(v);
+            vertexCnt++;
+            if (index == 0)
+                v->setFixed(true); // 将这个点的位置固定，不做优化。固定第一帧节点作为初值
+        } else if (name == "EDGE_SE3:QUAT") {
+            // SE3-SE3 边
+            g2o::EdgeSE3 *e = new g2o::EdgeSE3();
+            int idx1, idx2;     // 关联的两个顶点
+            fin >> idx1 >> idx2;
+            e->setId(edgeCnt++);
+            e->setVertex(0, optimizer.vertices()[idx1]); // 顶点的ID
+            e->setVertex(1, optimizer.vertices()[idx2]); // 顶点的ID
+            e->read(fin);  // 从 fin 流中读取 7 个数（平移向量 + 四元数）即 两个节点之间的运动估计。
+            optimizer.addEdge(e);  // 为什么没有使用 信息矩阵的数据 ？？？？？
+        }
+        if (!fin.good()) break;
+    }
+
+    cout << "read total " << vertexCnt << " vertices, " << edgeCnt << " edges." << endl;
+
+    cout << "optimizing ..." << endl;
+    optimizer.initializeOptimization();
+    optimizer.optimize(30);
+
+    cout << "saving optimization results ..." << endl;
+    optimizer.save("result.g2o");
+
+    return 0;
+}
